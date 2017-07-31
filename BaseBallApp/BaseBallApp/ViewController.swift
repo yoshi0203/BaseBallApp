ac0031f22e7342e7032143e66522cc11ac388788
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2017-08-01 01:08:28 +0900

    打率計算部分を追加

--- BaseBallApp/BaseBallApp/ViewController.swift
+++ BaseBallApp/BaseBallApp/ViewController.swift
@@ -25,6 +25,17 @@ class ViewController: UIViewController {
             atHits.text = "0"
         }
         
+        let calcBats = Double(atBats.text!)
+        let calcHits = Double(atHits.text!)
+        
+//        打席数と安打数が0の場合にNaNになるのを防ぐ
+        if calcBats == 0 && calcHits == 0 {
+            calcAverage.text = String(format: "%.3f" , 0)
+//        その他は小数第3位で四捨五入で表示
+        }else{
+            let ansAvr = ((calcHits! * 100) / calcBats! ) * 0.01
+            calcAverage.text = String(format: "%.3f" , round(ansAvr * 1000) * 0.001)
+        }
     }
     
     //    画面タップでキーボードを隠す
